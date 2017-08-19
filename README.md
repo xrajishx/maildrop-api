06c00277ab2f319940eee3526044e15e2e2d4ab9
Rajish Vijayan <rajishav16@gmail.com>
2017-08-19 23:58:51 +0530

    test(releasing): Improved tests. Added travis config, conventional commit and semantic release
    #1

--- README.md
+++ README.md
@@ -1,11 +1,49 @@
 # maildrop-api
 Wrapper module around [maildrop.cc](http://maildrop.cc) API.
-### Development
-###### Clone the project.
+### Installation
 ```bash
-git clone https://github.com/xrajishx/maildrop-api.git && cd maildrop-api
+npm install maildrop-api
 ```
-###### Install dependencies.
+### Usage
+```javascript
+const maildrop = require('maildrop-api');
+
+const mailBox = 'john';
+const emailId = 'ehFA9k';
+
+// Get inbox
+maildrop.getInbox(mailBox).then(emails => {
+  console.log(emails);
+});
+
+// Get email
+maildrop.getEmail(mailBox, emailId).then(email => {
+  console.log(email);
+});
+
+// Delete email
+maildrop.deleteEmail(mailBox, emailId).then(result => {
+  console.log(result);
+});
+
+// Suggestion API
+maildrop.suggestion().then(suggestion => {
+  console.log(suggestion);
+});
+
+// Blocked API
+maildrop.blocked().then(blocked => {
+  console.log(blocked);
+});
+```
+### Development
+Clone the project and install dependencies:
 ```bash
-npm install
+$ git clone https://github.com/xrajishx/maildrop-api.git
+$ cd maildrop-api
+$ npm install
+```
+Test:
+```bash
+$ npm test
 ```
--- test/test.js
+++ test/test.js
@@ -10,49 +10,57 @@ chai.should();
 const mailBox = 'john';
 
 describe('suggestion()', () => {
-  it('should return without error', () => {
-    return maildrop.suggestion().should.eventually.be.fulfilled;
+  it('should be fulfilled with a string without error', () => {
+    return maildrop.suggestion()
+      .should.eventually.be.fulfilled.and.be.a('string');
   });
 });
 
 describe('blocked()', () => {
-  it('should return without error', () => {
-    return maildrop.blocked().should.eventually.be.fulfilled;
+  it('should be fulfilled with a number without error', () => {
+    return maildrop.blocked()
+      .should.eventually.be.fulfilled.and.be.a('number');
   });
 });
 
 describe('getInbox(mailBox)', () => {
-  it('should be fulfilled without error', () => {
-    return maildrop.getInbox(mailBox).should.eventually.be.fulfilled;
+  it('should be fulfilled with an array without error', () => {
+    return maildrop.getInbox(mailBox)
+      .should.eventually.be.fulfilled.and.be.an('array');
   });
 });
 
 describe('getInbox()', () => {
   it('should be rejected with error', () => {
-    return maildrop.getInbox().should.eventually.be.rejected;
+    return maildrop.getInbox()
+      .should.eventually.be.rejected.and.be.an('error');
   });
 });
 
 describe('getEmail(mailBox, mailBox)', () => {
-  it('should be fulfilled without error', () => {
-    return maildrop.getEmail(mailBox, mailBox).should.eventually.be.fulfilled;
+  it('should be fulfilled with an object without error', () => {
+    return maildrop.getEmail(mailBox, mailBox)
+      .should.eventually.be.fulfilled.and.be.an('object');
   });
 });
 
 describe('getEmail()', () => {
   it('should be rejected with error', () => {
-    return maildrop.getEmail().should.eventually.be.rejected;
+    return maildrop.getEmail()
+      .should.eventually.be.rejected.and.be.an('error');
   });
 });
 
 describe('deleteEmail(mailBox, mailBox)', () => {
-  it('should be fulfilled without error', () => {
-    return maildrop.deleteEmail(mailBox, mailBox).should.eventually.be.fulfilled;
+  it('should be fulfilled with an object without error', () => {
+    return maildrop.deleteEmail(mailBox, mailBox)
+      .should.eventually.be.fulfilled.and.be.an('object');
   });
 });
 
 describe('deleteEmail()', () => {
   it('should be rejected with error', () => {
-    return maildrop.deleteEmail().should.eventually.be.rejected;
+    return maildrop.deleteEmail()
+      .should.eventually.be.rejected.and.be.an('error');
   });
 });
